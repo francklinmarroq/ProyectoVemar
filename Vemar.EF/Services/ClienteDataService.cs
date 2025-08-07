7579b8e211061e5d4917d0fb005411f4d2d69076
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-08-07 12:14:36 -0600

    - Formulario para detalles de cliente

---
+++ Vemar.EF/Services/ClienteDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vemar.Domain;
+
+namespace Vemar.EF.Services
+{
+ //Revisar video 9 del curso de mvvm de SingletonSean para ver implementacion
+}
